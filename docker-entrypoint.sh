40cb3cb0419873501e1b4687526ee321859b23de
Jørgen Saanum <43155457+Jorgsaa@users.noreply.github.com>
2022-03-07 15:14:37 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
-set -eou pipefail
+
+# Set database config from Heroku DATABASE_URL
+if [ "$DATABASE_URL" != "" ] && [ "$DB_ADDR" == "" ]; then
+    echo "Found database configuration in DATABASE_URL=$DATABASE_URL"
+
+    regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
+    if [[ $DATABASE_URL =~ $regex ]]; then
+        export DB_ADDR=${BASH_REMATCH[3]}
+        export DB_PORT=${BASH_REMATCH[4]}
+        export DB_DATABASE=${BASH_REMATCH[5]}
+        export DB_USER=${BASH_REMATCH[1]}
+        export DB_PASSWORD=${BASH_REMATCH[2]}
+
+        echo "DB_ADDR=$DB_ADDR, DB_PORT=$DB_PORT, DB_DATABASE=$DB_DATABASE, DB_USER=$DB_USER, DB_PASSWORD=$DB_PASSWORD"
+        export DB_VENDOR=postgres
+    fi
+
+fi
 
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
 #  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
 file_env() {
-    local var="$1"
-    local fileVar="${var}_FILE"
-    local def="${2:-}"
-    if [[ ${!var:-} && ${!fileVar:-} ]]; then
-        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-        exit 1
-    fi
-    local val="$def"
-    if [[ ${!var:-} ]]; then
-        val="${!var}"
-    elif [[ ${!fileVar:-} ]]; then
-        val="$(< "${!fileVar}")"
-    fi
-
-    if [[ -n $val ]]; then
-        export "$var"="$val"
-    fi
-
-    unset "$fileVar"
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
 }
-
-SYS_PROPS=""
 
 ##################
 # Add admin user #
@@ -36,31 +47,23 @@ SYS_PROPS=""
 file_env 'KEYCLOAK_USER'
 file_env 'KEYCLOAK_PASSWORD'
 
-if [[ -n ${KEYCLOAK_USER:-} && -n ${KEYCLOAK_PASSWORD:-} ]]; then
-    /opt/jboss/keycloak/bin/add-user-keycloak.sh --user "$KEYCLOAK_USER" --password "$KEYCLOAK_PASSWORD"
+if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
+    /opt/jboss/keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
 
 ############
 # Hostname #
 ############
 
-if [[ -n ${KEYCLOAK_FRONTEND_URL:-} ]]; then
-    SYS_PROPS+="-Dkeycloak.frontendUrl=$KEYCLOAK_FRONTEND_URL"
-fi
+if [ "$KEYCLOAK_HOSTNAME" != "" ]; then
+    SYS_PROPS="-Dkeycloak.hostname.provider=fixed -Dkeycloak.hostname.fixed.hostname=$KEYCLOAK_HOSTNAME"
 
-if [[ -n ${KEYCLOAK_HOSTNAME:-} ]]; then
-    SYS_PROPS+=" -Dkeycloak.hostname.provider=fixed -Dkeycloak.hostname.fixed.hostname=$KEYCLOAK_HOSTNAME"
-
-    if [[ -n ${KEYCLOAK_HTTP_PORT:-} ]]; then
+    if [ "$KEYCLOAK_HTTP_PORT" != "" ]; then
         SYS_PROPS+=" -Dkeycloak.hostname.fixed.httpPort=$KEYCLOAK_HTTP_PORT"
     fi
 
-    if [[ -n ${KEYCLOAK_HTTPS_PORT:-} ]]; then
+    if [ "$KEYCLOAK_HTTPS_PORT" != "" ]; then
         SYS_PROPS+=" -Dkeycloak.hostname.fixed.httpsPort=$KEYCLOAK_HTTPS_PORT"
-    fi
-
-    if [[ -n ${KEYCLOAK_ALWAYS_HTTPS:-} ]]; then
-            SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
     fi
 fi
 
@@ -68,7 +71,7 @@ fi
 # Realm import #
 ################
 
-if [[ -n ${KEYCLOAK_IMPORT:-} ]]; then
+if [ "$KEYCLOAK_IMPORT" ]; then
     SYS_PROPS+=" -Dkeycloak.import=$KEYCLOAK_IMPORT"
 fi
 
@@ -76,10 +79,10 @@ fi
 # JGroups bind options #
 ########################
 
-if [[ -z ${BIND:-} ]]; then
-    BIND=$(hostname --all-ip-addresses)
+if [ -z "$BIND" ]; then
+    BIND=$(hostname -i)
 fi
-if [[ -z ${BIND_OPTS:-} ]]; then
+if [ -z "$BIND_OPTS" ]; then
     for BIND_IP in $BIND
     do
         BIND_OPTS+=" -Djboss.bind.address=$BIND_IP -Djboss.bind.address.private=$BIND_IP "
@@ -87,25 +90,14 @@ if [[ -z ${BIND_OPTS:-} ]]; then
 fi
 SYS_PROPS+=" $BIND_OPTS"
 
-#########################################
-# Expose management console for metrics #
-#########################################
-
-if [[ -n ${KEYCLOAK_STATISTICS:-} ]] ; then
-    SYS_PROPS+=" -Djboss.bind.address.management=0.0.0.0"
-fi
-
 #################
 # Configuration #
 #################
 
 # If the server configuration parameter is not present, append the HA profile.
-if echo "$@" | grep -E -v -- '-c |-c=|--server-config |--server-config='; then
+if echo "$@" | egrep -v -- '-c |-c=|--server-config |--server-config='; then
     SYS_PROPS+=" -c=standalone-ha.xml"
 fi
-
-# Adding support for JAVA_OPTS_APPEND
-sed -i '$a\\n# Append to JAVA_OPTS. Necessary to prevent some values being omitted if JAVA_OPTS is defined directly\nJAVA_OPTS=\"\$JAVA_OPTS \$JAVA_OPTS_APPEND\"' /opt/jboss/keycloak/bin/standalone.conf
 
 ############
 # DB setup #
@@ -113,116 +105,67 @@ sed -i '$a\\n# Append to JAVA_OPTS. Necessary to prevent some values being omitt
 
 file_env 'DB_USER'
 file_env 'DB_PASSWORD'
+
 # Lower case DB_VENDOR
-if [[ -n ${DB_VENDOR:-} ]]; then
-  DB_VENDOR=$(echo "$DB_VENDOR" | tr "[:upper:]" "[:lower:]")
-fi
+DB_VENDOR=`echo $DB_VENDOR | tr A-Z a-z`
 
 # Detect DB vendor from default host names
-if [[ -z ${DB_VENDOR:-} ]]; then
+if [ "$DB_VENDOR" == "" ]; then
     if (getent hosts postgres &>/dev/null); then
         export DB_VENDOR="postgres"
     elif (getent hosts mysql &>/dev/null); then
         export DB_VENDOR="mysql"
     elif (getent hosts mariadb &>/dev/null); then
         export DB_VENDOR="mariadb"
-    elif (getent hosts oracle &>/dev/null); then
-        export DB_VENDOR="oracle"
-    elif (getent hosts mssql &>/dev/null); then
-        export DB_VENDOR="mssql"
-    elif (getent hosts h2 &>/dev/null); then
-        export DB_VENDOR="h2"
-        export DB_ADDR="h2"
     fi
 fi
 
 # Detect DB vendor from legacy `*_ADDR` environment variables
-if [[ -z ${DB_VENDOR:-} ]]; then
+if [ "$DB_VENDOR" == "" ]; then
     if (printenv | grep '^POSTGRES_ADDR=' &>/dev/null); then
         export DB_VENDOR="postgres"
     elif (printenv | grep '^MYSQL_ADDR=' &>/dev/null); then
         export DB_VENDOR="mysql"
     elif (printenv | grep '^MARIADB_ADDR=' &>/dev/null); then
         export DB_VENDOR="mariadb"
-    elif (printenv | grep '^ORACLE_ADDR=' &>/dev/null); then
-        export DB_VENDOR="oracle"
-    elif (printenv | grep '^MSSQL_ADDR=' &>/dev/null); then
-        export DB_VENDOR="mssql"
-    elif (printenv | grep '^H2_ADDR=' &>/dev/null); then
-        export DB_VENDOR="h2"
-        export DB_ADDR="h2"
     fi
 fi
 
 # Default to H2 if DB type not detected
-if [[ -z ${DB_VENDOR:-} ]]; then
+if [ "$DB_VENDOR" == "" ]; then
     export DB_VENDOR="h2"
 fi
 
-# if the DB_VENDOR is postgres then append port to the DB_ADDR
-function append_port_db_addr() {
-  local db_host_regex='^[a-zA-Z0-9]([a-zA-Z0-9]|-|.)*:[0-9]{4,5}$'
-  IFS=',' read -ra addresses <<< "$DB_ADDR"
-  DB_ADDR=""
-  for i in "${addresses[@]}"; do
-    if [[ $i =~ $db_host_regex ]]; then
-        DB_ADDR+=$i;
-    else
-        DB_ADDR+="${i}:${DB_PORT}";
-    fi
-    DB_ADDR+=","
-  done
-  DB_ADDR=$(echo $DB_ADDR | sed 's/.$//') # remove the last comma
-}
 # Set DB name
 case "$DB_VENDOR" in
     postgres)
-        DB_NAME="PostgreSQL"
-        if [[ -z ${DB_PORT:-} ]] ; then
-          DB_PORT="5432"
-        fi
-        append_port_db_addr
-        ;;
+        DB_NAME="PostgreSQL";;
     mysql)
         DB_NAME="MySQL";;
     mariadb)
         DB_NAME="MariaDB";;
-    mssql)
-        DB_NAME="Microsoft SQL Server";;
-    oracle)
-        DB_NAME="Oracle";;
     h2)
-        if [[ -z ${DB_ADDR:-} ]] ; then
-          DB_NAME="Embedded H2"
-        else
-          DB_NAME="H2"
-        fi;;
+        DB_NAME="Embedded H2";;
     *)
         echo "Unknown DB vendor $DB_VENDOR"
         exit 1
 esac
 
-if [ "$DB_VENDOR" != "mssql" ] && [ "$DB_VENDOR" != "h2" ]; then
-    # Append '?' in the beginning of the string if JDBC_PARAMS value isn't empty
-    JDBC_PARAMS=$(echo "${JDBC_PARAMS:-}" | sed '/^$/! s/^/?/')
-else
-    JDBC_PARAMS=${JDBC_PARAMS:-}
-fi
-
-export JDBC_PARAMS
+# Append '?' in the beggining of the string if JDBC_PARAMS value isn't empty
+export JDBC_PARAMS=$(echo ${JDBC_PARAMS} | sed '/^$/! s/^/?/')
 
 # Convert deprecated DB specific variables
 function set_legacy_vars() {
   local suffixes=(ADDR DATABASE USER PASSWORD PORT)
   for suffix in "${suffixes[@]}"; do
     local varname="$1_$suffix"
-    if [[ -n ${!varname:-} ]]; then
-      echo WARNING: "$varname" variable name is DEPRECATED replace with DB_"$suffix"
-      export DB_"$suffix=${!varname}"
+    if [ ${!varname} ]; then
+      echo WARNING: $varname variable name is DEPRECATED replace with DB_$suffix
+      export DB_$suffix=${!varname}
     fi
   done
 }
-set_legacy_vars "$(echo "$DB_VENDOR" | tr "[:upper:]" "[:lower:]")"
+set_legacy_vars `echo $DB_VENDOR | tr a-z A-Z`
 
 # Configure DB
 
@@ -233,25 +176,17 @@ echo ""
 echo "========================================================================="
 echo ""
 
-configured_file="/opt/jboss/configured"
-if [ ! -e "$configured_file" ]; then
-    touch "$configured_file"
-
-    if [ "$DB_NAME" != "Embedded H2" ]; then
-      /bin/sh /opt/jboss/tools/databases/change-database.sh $DB_VENDOR
-    fi
-	
-    /opt/jboss/tools/x509.sh
-    /opt/jboss/tools/jgroups.sh
-    /opt/jboss/tools/infinispan.sh
-    /opt/jboss/tools/statistics.sh
-    /opt/jboss/tools/vault.sh
-    /opt/jboss/tools/autorun.sh
+if [ "$DB_VENDOR" != "h2" ]; then
+    /bin/sh /opt/jboss/tools/databases/change-database.sh $DB_VENDOR
 fi
+
+/opt/jboss/tools/x509.sh
+/opt/jboss/tools/jgroups.sh $JGROUPS_DISCOVERY_PROTOCOL $JGROUPS_DISCOVERY_PROPERTIES
+/opt/jboss/tools/autorun.sh
 
 ##################
 # Start Keycloak #
 ##################
 
-exec /opt/jboss/keycloak/bin/standalone.sh $SYS_PROPS $@
+exec /opt/jboss/keycloak/bin/standalone.sh $SYS_PROPS $@ -Djboss.http.port=$PORT
 exit $?
